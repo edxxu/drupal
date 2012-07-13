8f8c6671246f1c4d3379b54bf32a2060b9c2192e
edxxu <jin.xu@insready.com>
2012-07-13 15:23:20 +0800

    a

---
+++ sites/default/files/js/js_dY5XYMDkMVlP9_cxSeFP_-HzO5OPiyP0QAtXv7XbAwk.js
@@ -0,0 +1,468 @@
+(function ($) {
+
+/**
+ * Attaches double-click behavior to toggle full path of Krumo elements.
+ */
+Drupal.behaviors.devel = {
+  attach: function (context, settings) {
+
+    // Add hint to footnote
+    $('.krumo-footnote .krumo-call').before('<img style="vertical-align: middle;" title="Click to expand. Double-click to show path." src="' + Drupal.settings.basePath + 'misc/help.png"/>');
+
+    var krumo_name = [];
+    var krumo_type = [];
+
+    function krumo_traverse(el) {
+      krumo_name.push($(el).html());
+      krumo_type.push($(el).siblings('em').html().match(/\w*/)[0]);
+
+      if ($(el).closest('.krumo-nest').length > 0) {
+        krumo_traverse($(el).closest('.krumo-nest').prev().find('.krumo-name'));
+      }
+    }
+
+    $('.krumo-child > div:first-child', context).dblclick(
+      function(e) {
+        if ($(this).find('> .krumo-php-path').length > 0) {
+          // Remove path if shown.
+          $(this).find('> .krumo-php-path').remove();
+        }
+        else {
+          // Get elements.
+          krumo_traverse($(this).find('> a.krumo-name'));
+
+          // Create path.
+          var krumo_path_string = '';
+          for (var i = krumo_name.length - 1; i >= 0; --i) {
+            // Start element.
+            if ((krumo_name.length - 1) == i)
+              krumo_path_string += '$' + krumo_name[i];
+
+            if (typeof krumo_name[(i-1)] !== 'undefined') {
+              if (krumo_type[i] == 'Array') {
+                krumo_path_string += "[";
+                if (!/^\d*$/.test(krumo_name[(i-1)]))
+                  krumo_path_string += "'";
+                krumo_path_string += krumo_name[(i-1)];
+                if (!/^\d*$/.test(krumo_name[(i-1)]))
+                  krumo_path_string += "'";
+                krumo_path_string += "]";
+              }
+              if (krumo_type[i] == 'Object')
+                krumo_path_string += '->' + krumo_name[(i-1)];
+            }
+          }
+          $(this).append('<div class="krumo-php-path" style="font-family: Courier, monospace; font-weight: bold;">' + krumo_path_string + '</div>');
+
+          // Reset arrays.
+          krumo_name = [];
+          krumo_type = [];
+        }
+      }
+    );
+  }
+};
+
+})(jQuery);
+;
+(function($) {
+
+Drupal.admin = Drupal.admin || {};
+Drupal.admin.behaviors = Drupal.admin.behaviors || {};
+Drupal.admin.hashes = Drupal.admin.hashes || {};
+
+/**
+ * Core behavior for Administration menu.
+ *
+ * Test whether there is an administration menu is in the output and execute all
+ * registered behaviors.
+ */
+Drupal.behaviors.adminMenu = {
+  attach: function (context, settings) {
+    // Initialize settings.
+    settings.admin_menu = $.extend({
+      suppress: false,
+      margin_top: false,
+      position_fixed: false,
+      tweak_modules: false,
+      tweak_permissions: false,
+      tweak_tabs: false,
+      destination: '',
+      basePath: settings.basePath,
+      hash: 0,
+      replacements: {}
+    }, settings.admin_menu || {});
+    // Check whether administration menu should be suppressed.
+    if (settings.admin_menu.suppress) {
+      return;
+    }
+    var $adminMenu = $('#admin-menu:not(.admin-menu-processed)', context);
+    // Client-side caching; if administration menu is not in the output, it is
+    // fetched from the server and cached in the browser.
+    if (!$adminMenu.length && settings.admin_menu.hash) {
+      Drupal.admin.getCache(settings.admin_menu.hash, function (response) {
+          if (typeof response == 'string' && response.length > 0) {
+            $('body', context).prepend(response);
+          }
+          var $adminMenu = $('#admin-menu:not(.admin-menu-processed)', context);
+          // Apply our behaviors.
+          Drupal.admin.attachBehaviors(context, settings, $adminMenu);
+      });
+    }
+    // If the menu is in the output already, this means there is a new version.
+    else {
+      // Apply our behaviors.
+      Drupal.admin.attachBehaviors(context, settings, $adminMenu);
+    }
+  }
+};
+
+/**
+ * Collapse fieldsets on Modules page.
+ */
+Drupal.behaviors.adminMenuCollapseModules = {
+  attach: function (context, settings) {
+    if (settings.admin_menu.tweak_modules) {
+      $('#system-modules fieldset:not(.collapsed)', context).addClass('collapsed');
+    }
+  }
+};
+
+/**
+ * Collapse modules on Permissions page.
+ */
+Drupal.behaviors.adminMenuCollapsePermissions = {
+  attach: function (context, settings) {
+    if (settings.admin_menu.tweak_permissions) {
+      // Freeze width of first column to prevent jumping.
+      $('#permissions th:first', context).css({ width: $('#permissions th:first', context).width() });
+      // Attach click handler.
+      $('#permissions tr:has(td.module)', context).once('admin-menu-tweak-permissions', function () {
+        var $module = $(this);
+        $module.bind('click.admin-menu', function () {
+          // @todo Replace with .nextUntil() in jQuery 1.4.
+          $module.nextAll().each(function () {
+            var $row = $(this);
+            if ($row.is(':has(td.module)')) {
+              return false;
+            }
+            $row.toggleClass('element-hidden');
+          });
+        });
+      }).trigger('click.admin-menu');
+    }
+  }
+};
+
+/**
+ * Apply margin to page.
+ *
+ * Note that directly applying marginTop does not work in IE. To prevent
+ * flickering/jumping page content with client-side caching, this is a regular
+ * Drupal behavior.
+ */
+Drupal.behaviors.adminMenuMarginTop = {
+  attach: function (context, settings) {
+    if (!settings.admin_menu.suppress && settings.admin_menu.margin_top) {
+      $('body:not(.admin-menu)', context).addClass('admin-menu');
+    }
+  }
+};
+
+/**
+ * Retrieve content from client-side cache.
+ *
+ * @param hash
+ *   The md5 hash of the content to retrieve.
+ * @param onSuccess
+ *   A callback function invoked when the cache request was successful.
+ */
+Drupal.admin.getCache = function (hash, onSuccess) {
+  if (Drupal.admin.hashes.hash !== undefined) {
+    return Drupal.admin.hashes.hash;
+  }
+  $.ajax({
+    cache: true,
+    type: 'GET',
+    dataType: 'text', // Prevent auto-evaluation of response.
+    global: false, // Do not trigger global AJAX events.
+    url: Drupal.settings.admin_menu.basePath.replace(/admin_menu/, 'js/admin_menu/cache/' + hash),
+    success: onSuccess,
+    complete: function (XMLHttpRequest, status) {
+      Drupal.admin.hashes.hash = status;
+    }
+  });
+};
+
+/**
+ * @defgroup admin_behaviors Administration behaviors.
+ * @{
+ */
+
+/**
+ * Attach administrative behaviors.
+ */
+Drupal.admin.attachBehaviors = function (context, settings, $adminMenu) {
+  if ($adminMenu.length) {
+    $adminMenu.addClass('admin-menu-processed');
+    $.each(Drupal.admin.behaviors, function() {
+      this(context, settings, $adminMenu);
+    });
+  }
+};
+
+/**
+ * Apply 'position: fixed'.
+ */
+Drupal.admin.behaviors.positionFixed = function (context, settings, $adminMenu) {
+  if (settings.admin_menu.position_fixed) {
+    $adminMenu.addClass('admin-menu-position-fixed');
+    $adminMenu.css('position', 'fixed');
+  }
+};
+
+/**
+ * Move page tabs into administration menu.
+ */
+Drupal.admin.behaviors.pageTabs = function (context, settings, $adminMenu) {
+  if (settings.admin_menu.tweak_tabs) {
+    $('ul.tabs.primary li', context).addClass('admin-menu-tab').appendTo('#admin-menu-wrapper > ul');
+    $('ul.tabs.secondary', context).appendTo('#admin-menu-wrapper > ul > li.admin-menu-tab.active').removeClass('secondary');
+    $('ul.tabs.primary', context).remove();
+  }
+};
+
+/**
+ * Perform dynamic replacements in cached menu.
+ */
+Drupal.admin.behaviors.replacements = function (context, settings, $adminMenu) {
+  for (var item in settings.admin_menu.replacements) {
+    $(item, $adminMenu).html(settings.admin_menu.replacements[item]);
+  }
+};
+
+/**
+ * Inject destination query strings for current page.
+ */
+Drupal.admin.behaviors.destination = function (context, settings, $adminMenu) {
+  if (settings.admin_menu.destination) {
+    $('a.admin-menu-destination', $adminMenu).each(function() {
+      this.search += (!this.search.length ? '?' : '&') + Drupal.settings.admin_menu.destination;
+    });
+  }
+};
+
+/**
+ * Apply JavaScript-based hovering behaviors.
+ *
+ * @todo This has to run last.  If another script registers additional behaviors
+ *   it will not run last.
+ */
+Drupal.admin.behaviors.hover = function (context, settings, $adminMenu) {
+  // Hover emulation for IE 6.
+  if ($.browser.msie && parseInt(jQuery.browser.version) == 6) {
+    $('li', $adminMenu).hover(
+      function () {
+        $(this).addClass('iehover');
+      },
+      function () {
+        $(this).removeClass('iehover');
+      }
+    );
+  }
+
+  // Delayed mouseout.
+  $('li.expandable', $adminMenu).hover(
+    function () {
+      // Stop the timer.
+      clearTimeout(this.sfTimer);
+      // Display child lists.
+      $('> ul', this)
+        .css({left: 'auto', display: 'block'})
+        // Immediately hide nephew lists.
+        .parent().siblings('li').children('ul').css({left: '-999em', display: 'none'});
+    },
+    function () {
+      // Start the timer.
+      var uls = $('> ul', this);
+      this.sfTimer = setTimeout(function () {
+        uls.css({left: '-999em', display: 'none'});
+      }, 400);
+    }
+  );
+};
+
+/**
+ * @} End of "defgroup admin_behaviors".
+ */
+
+})(jQuery);
+;
+(function($) {
+
+Drupal.admin = Drupal.admin || {};
+Drupal.admin.behaviors = Drupal.admin.behaviors || {};
+
+/**
+ * @ingroup admin_behaviors
+ * @{
+ */
+
+/**
+ * Apply active trail highlighting based on current path.
+ *
+ * @todo Not limited to toolbar; move into core?
+ */
+Drupal.admin.behaviors.toolbarActiveTrail = function (context, settings, $adminMenu) {
+  if (settings.admin_menu.toolbar && settings.admin_menu.toolbar.activeTrail) {
+    $adminMenu.find('> div > ul > li > a[href="' + settings.admin_menu.toolbar.activeTrail + '"]').addClass('active-trail');
+  }
+};
+
+/**
+ * @} End of "defgroup admin_behaviors".
+ */
+
+})(jQuery);
+;
+(function ($) {
+
+/**
+ * Show/hide the 'Email site administrator when updates are available' checkbox
+ * on the install page.
+ */
+Drupal.hideEmailAdministratorCheckbox = function () {
+  // Make sure the secondary box is shown / hidden as necessary on page load.
+  if ($('#edit-update-status-module-1').is(':checked')) {
+    $('.form-item-update-status-module-2').show();
+  }
+  else {
+    $('.form-item-update-status-module-2').hide();
+  }
+
+  // Toggle the display as necessary when the checkbox is clicked.
+  $('#edit-update-status-module-1').change( function () {
+    $('.form-item-update-status-module-2').toggle();
+  });
+};
+
+/**
+ * Internal function to check using Ajax if clean URLs can be enabled on the
+ * settings page.
+ *
+ * This function is not used to verify whether or not clean URLs
+ * are currently enabled.
+ */
+Drupal.behaviors.cleanURLsSettingsCheck = {
+  attach: function (context, settings) {
+    // This behavior attaches by ID, so is only valid once on a page.
+    // Also skip if we are on an install page, as Drupal.cleanURLsInstallCheck will handle
+    // the processing.
+    if (!($('#edit-clean-url').length) || $('#edit-clean-url.install').once('clean-url').length) {
+      return;
+    }
+    var url = settings.basePath + 'admin/config/search/clean-urls/check';
+    $.ajax({
+      url: location.protocol + '//' + location.host + url,
+      dataType: 'json',
+      success: function () {
+        // Check was successful. Redirect using a "clean URL". This will force the form that allows enabling clean URLs.
+        location = settings.basePath +"admin/config/search/clean-urls";
+      }
+    });
+  }
+};
+
+/**
+ * Internal function to check using Ajax if clean URLs can be enabled on the
+ * install page.
+ *
+ * This function is not used to verify whether or not clean URLs
+ * are currently enabled.
+ */
+Drupal.cleanURLsInstallCheck = function () {
+  var url = location.protocol + '//' + location.host + Drupal.settings.basePath + 'admin/config/search/clean-urls/check';
+  // Submit a synchronous request to avoid database errors associated with
+  // concurrent requests during install.
+  $.ajax({
+    async: false,
+    url: url,
+    dataType: 'json',
+    success: function () {
+      // Check was successful.
+      $('#edit-clean-url').attr('value', 1);
+    }
+  });
+};
+
+/**
+ * When a field is filled out, apply its value to other fields that will likely
+ * use the same value. In the installer this is used to populate the
+ * administrator e-mail address with the same value as the site e-mail address.
+ */
+Drupal.behaviors.copyFieldValue = {
+  attach: function (context, settings) {
+    for (var sourceId in settings.copyFieldValue) {
+      $('#' + sourceId, context).once('copy-field-values').bind('blur', function () {
+        // Get the list of target fields.
+        var targetIds = settings.copyFieldValue[sourceId];
+        // Add the behavior to update target fields on blur of the primary field.
+        for (var delta in targetIds) {
+          var targetField = $('#' + targetIds[delta]);
+          if (targetField.val() == '') {
+            targetField.val(this.value);
+          }
+        }
+      });
+    }
+  }
+};
+
+/**
+ * Show/hide custom format sections on the regional settings page.
+ */
+Drupal.behaviors.dateTime = {
+  attach: function (context, settings) {
+    for (var fieldName in settings.dateTime) {
+      if (settings.dateTime.hasOwnProperty(fieldName)) {
+        (function (fieldSettings, fieldName) {
+          var source = '#edit-' + fieldName;
+          var suffix = source + '-suffix';
+
+          // Attach keyup handler to custom format inputs.
+          $('input' + source, context).once('date-time').keyup(function () {
+            var input = $(this);
+            var url = fieldSettings.lookup + (/\?q=/.test(fieldSettings.lookup) ? '&format=' : '?format=') + encodeURIComponent(input.val());
+            $.getJSON(url, function (data) {
+              $(suffix).empty().append(' ' + fieldSettings.text + ': <em>' + data + '</em>');
+            });
+          });
+        })(settings.dateTime[fieldName], fieldName);
+      }
+    }
+  }
+};
+
+ /**
+ * Show/hide settings for page caching depending on whether page caching is
+ * enabled or not.
+ */
+Drupal.behaviors.pageCache = {
+  attach: function (context, settings) {
+    $('#edit-cache-0', context).change(function () {
+      $('#page-compression-wrapper').hide();
+      $('#cache-error').hide();
+    });
+    $('#edit-cache-1', context).change(function () {
+      $('#page-compression-wrapper').show();
+      $('#cache-error').hide();
+    });
+    $('#edit-cache-2', context).change(function () {
+      $('#page-compression-wrapper').show();
+      $('#cache-error').show();
+    });
+  }
+};
+
+})(jQuery);
+;
